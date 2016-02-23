36cc8288ab76cf77a9873547e10c183c4027d7b4
Tin Rabuzin <rabuzin@kth.se>
2016-02-23 10:57:24 +0100

    Adding the code to run SecondOrder example during setup

--- Sources/setup_rapid.m
+++ Sources/setup_rapid.m
@@ -31,6 +31,7 @@ savepath
 
 % Check of RaPId dependencies
 run('.\install\check_installed')
-
+disp('======= Running the test example =======');
+run('.\install\run_example')
 % En of setup, run the GUI
 run_rapid_gui
